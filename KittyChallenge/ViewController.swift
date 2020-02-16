8b6e4acd7f173098cb94863ccdca11d2edabd18a
Stuti Dobhal <stuti.dobhal@circ.com>
2020-02-16 22:02:44 +0100

    Initial Commit

---
+++ KittyChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KittyChallenge
+//
+//  Created by Stuti Dobhal on 16.02.20.
+//  Copyright © 2020 Stuti Dobhal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
